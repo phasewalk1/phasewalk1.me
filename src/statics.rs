7b5c1697f44b7b54d6f7449c0e42d38124aa0f61
phasewalk1 <staticanne@skiff.com>
2023-06-08 12:03:40 -0700

    fix persy link

--- src/statics.rs
+++ src/statics.rs
@@ -5,7 +5,7 @@ pub(super) mod refs {
     pub(crate) static MIRROR: StaticStr =
         "https://mirror.xyz/0x497437B2aC4B2D408138bE772233E3F2dF8b7064";
     pub(crate) static GITHUB: StaticStr = "https://github.com/phasewalk1";
-    pub(crate) static PERSY: StaticStr = "https://phasewalk1.github.io";
+    pub(crate) static PERSY: StaticStr = "https://phasewalk.xyz";
     pub(crate) static TWITTER: StaticStr = "https://twitter.com/phasewalk1";
     pub(crate) static MAILTO: StaticStr = "mailto:kat@phasewalk1.net";
 }
